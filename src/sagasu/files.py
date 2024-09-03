877f0565d88586034da73597643fec806c8e8880
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-09-03 08:23:09 +0200

    refactor: read files async

--- src/sagasu/files.py
+++ src/sagasu/files.py
@@ -1,39 +1,43 @@
 from pathlib import Path
 import mimetypes
+from aiofile import async_open
 import frontmatter
 from sagasu.utils import tokenize
 
 
-def list_files_in_dir(path: Path) -> list[Path]:
+def list_files_in_dir(path: Path) -> list[Path] | None:
     if not path.is_dir():
         # TODO: raise
         print("Error: path provided is not a directoy")
+        return None
 
     files = [file.resolve() for file in path.rglob("*") if file.is_file()]
 
     return files
 
 
-def read_file(file: Path) -> tuple[str, list[str], dict] | None:
+async def read_file(file: Path) -> tuple[str, list[str], dict] | None:
     mime_type, _ = mimetypes.guess_type(file)
 
     try:
         if mime_type == "text/plain":
-            return _read_plain_text_file(file)
+            return await _read_plain_text_file(file)
         if mime_type == "text/markdown":
-            return _read_markdown_file(file)
+            return await _read_markdown_file(file)
         else:
             print(f"Unsupported file format {mime_type}")
     except Exception as e:
         print(f"Error reading file: {e}")
 
 
-def _read_plain_text_file(file: Path) -> tuple[str, list[str], dict] | None:
-    with file.open("r", encoding="utf-8") as f:
-        content = f.read()
+async def _read_plain_text_file(file: Path) -> tuple[str, list[str], dict] | None:
+    async with async_open(file, "r") as af:
+        content = await af.read()
         return (content, tokenize(content), {})
 
 
-def _read_markdown_file(file: Path) -> tuple[str, list[str], dict] | None:
-    post = frontmatter.load(f"{file.resolve()}")
-    return (post.content, tokenize(post.content), post.metadata)
+async def _read_markdown_file(file: Path) -> tuple[str, list[str], dict] | None:
+    async with async_open(file, "r") as af:
+        content = await af.read()
+        post = frontmatter.loads(content)
+        return (post.content, tokenize(post.content), {})
--- tests/test_files.py
+++ tests/test_files.py
@@ -1,18 +1,21 @@
+import pytest
 from pathlib import Path
 from sagasu.files import _read_plain_text_file, _read_markdown_file
 
 TEST_FILES_DIR = Path(__file__).parent / "samples"
 
 
-def test_read_plain_text_file():
+@pytest.mark.asyncio
+async def test_read_plain_text_file():
     test_file = TEST_FILES_DIR / "short.txt"
 
-    result = _read_plain_text_file(test_file)
+    result = await _read_plain_text_file(test_file)
     assert result is not None
 
 
-def test_markdown_file():
+@pytest.mark.asyncio
+async def test_markdown_file():
     test_file = TEST_FILES_DIR / "short.md"
 
-    result = _read_markdown_file(test_file)
+    result = await _read_markdown_file(test_file)
     assert result is not None

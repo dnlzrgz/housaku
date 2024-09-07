00ffad60b21238b7ad5d06f5819afb341d1e9bbb
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-09-07 11:00:24 +0200

    feat(files): add support for pdf files

--- src/sagasu/files.py
+++ src/sagasu/files.py
@@ -1,43 +1,49 @@
 from pathlib import Path
 import mimetypes
 from aiofile import async_open
+from pypdf import PdfReader
 import frontmatter
 from sagasu.utils import tokenize
 
 
-def list_files_in_dir(path: Path) -> list[Path] | None:
+def list_files(path: Path) -> list[Path]:
     if not path.is_dir():
-        # TODO: raise
-        print("Error: path provided is not a directoy")
-        return None
+        raise Exception(f"Error: path {path} is not a directoy")
 
-    files = [file.resolve() for file in path.rglob("*") if file.is_file()]
-
-    return files
+    return [file.resolve() for file in path.rglob("*") if file.is_file()]
 
 
-async def read_file(file: Path) -> tuple[str, list[str], dict] | None:
+async def extract_tokens(file: Path) -> list[str]:
     mime_type, _ = mimetypes.guess_type(file)
 
-    try:
-        if mime_type == "text/plain":
-            return await _read_plain_text_file(file)
-        if mime_type == "text/markdown":
-            return await _read_markdown_file(file)
-        else:
-            print(f"Unsupported file format {mime_type}")
-    except Exception as e:
-        print(f"Error reading file: {e}")
+    if mime_type == "text/plain":
+        return await read_txt(file)
+    if mime_type == "text/markdown":
+        return await read_md(file)
+    if mime_type == "application/pdf":
+        return await read_pdf(file)
+    else:
+        raise Exception(f"Unsupported file format {mime_type}")
 
 
-async def _read_plain_text_file(file: Path) -> tuple[str, list[str], dict] | None:
+async def read_txt(file: Path) -> list[str]:
     async with async_open(file, "r") as af:
         content = await af.read()
-        return (content, tokenize(content), {})
+        return tokenize(content)
 
 
-async def _read_markdown_file(file: Path) -> tuple[str, list[str], dict] | None:
+async def read_md(file: Path) -> list[str]:
     async with async_open(file, "r") as af:
         content = await af.read()
         post = frontmatter.loads(content)
-        return (post.content, tokenize(post.content), {})
+        return tokenize(post.content)
+
+
+async def read_pdf(file: Path) -> list[str]:
+    tokens = []
+
+    reader = PdfReader(file.resolve())
+    for page in reader.pages:
+        tokens.extend(tokenize(page.extract_text()))
+
+    return tokens
--- tests/test_files.py
+++ tests/test_files.py
@@ -1,21 +1,47 @@
 import pytest
 from pathlib import Path
-from sagasu.files import _read_plain_text_file, _read_markdown_file
+from sagasu.files import read_txt, read_md, read_pdf
 
-TEST_FILES_DIR = Path(__file__).parent / "samples"
-
-
-@pytest.mark.asyncio
-async def test_read_plain_text_file():
-    test_file = TEST_FILES_DIR / "short.txt"
-
-    result = await _read_plain_text_file(test_file)
-    assert result is not None
+TEST_FILES_DIR = Path(__file__).parent / "examples"
 
 
 @pytest.mark.asyncio
-async def test_markdown_file():
-    test_file = TEST_FILES_DIR / "short.md"
+async def test_read_txt():
+    for test_file in TEST_FILES_DIR.glob("*.txt"):
+        tokens = await read_txt(test_file)
+        assert len(tokens) > 0
 
-    result = await _read_markdown_file(test_file)
-    assert result is not None
+
+@pytest.mark.asyncio
+async def test_read_md():
+    for test_file in TEST_FILES_DIR.glob("*.md"):
+        tokens = await read_md(test_file)
+        assert len(tokens) > 0
+
+
+@pytest.mark.asyncio
+async def test_read_pdf():
+    for test_file in TEST_FILES_DIR.glob("*.pdf"):
+        tokens = await read_pdf(test_file)
+        assert len(tokens) > 0
+
+
+@pytest.mark.asyncio
+async def test_bench_read_txt(benchmark):
+    test_file = TEST_FILES_DIR / "gutenberg_moby_dick.txt"
+    result = await benchmark(read_txt, test_file)
+    assert result
+
+
+@pytest.mark.asyncio
+async def test_bench_read_md(benchmark):
+    test_file = TEST_FILES_DIR / "daring_fireball_markdown_syntax.md"
+    result = await benchmark(read_md, test_file)
+    assert result
+
+
+@pytest.mark.asyncio
+async def test_bench_read_pdf(benchmark):
+    test_file = TEST_FILES_DIR / "gutenberg_the_modern_prometheus.pdf"
+    result = await benchmark(read_pdf, test_file)
+    assert result

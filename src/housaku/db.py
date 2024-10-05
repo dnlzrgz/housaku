a4e458738a1980249460ed22f5cd77af3bd1485b
dnlzrgz <24715931+dnlzrgz@users.noreply.github.com>
2024-10-05 15:56:03 +0200

    feat(db): add transaction mode immediate

--- src/housaku/db.py
+++ src/housaku/db.py
@@ -12,12 +12,14 @@ def init_db(sqlite_url: str) -> None:
     );
     """)
 
-    cursor.execute("PRAGMA foreign_keys=ON;")
-    cursor.execute("PRAGMA journal_mode=WAL;")
-    cursor.execute("PRAGMA synchronous=NORMAL;")
-    cursor.execute("PRAGMA busy_timeout=5000;")
-    cursor.execute("PRAGMA temp_store=MEMORY;")
+    cursor.execute("PRAGMA journal_mode = WAL;")
+    cursor.execute("PRAGMA foreign_keys = ON;")
+    cursor.execute("PRAGMA synchronous = NORMAL;")
+    cursor.execute("PRAGMA busy_timeout = 5000;")
+    cursor.execute("PRAGMA temp_store = MEMORY;")
     cursor.execute("PRAGMA cache_size = 2000;")
+
+    cursor.execute("PRAGMA transaction_mode = IMMEDIATE;")
 
     conn.commit()
     conn.close()
